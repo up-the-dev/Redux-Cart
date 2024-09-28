25626dee0798f797d80a2eb56239854069b22ffa
Umesh Pawar <umesh.a.pawar2002@gmail.com>
2024-09-28 11:30:33 +0530

    initial setup with vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className=' font-extralight text-4xl'>
+        Welcome
+      </div>
+    </>
+  )
+}
+
+export default App
